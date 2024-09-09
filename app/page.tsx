636706e1cb060573d6c4981ce57056515a78bdba
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-09-09 12:47:32 -0500

    start

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
+import { Briefcase, Calendar, CheckCircle, Code } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="py-2 container mx-auto px-4 lg:w-3/4 2xl:w-1/2">
+      <div className="flex flex-col gap-3">
+        <div>
+          <h1>Luke Davies</h1>
+          <p>Founder QB Logs, Student-Athlete</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <h2>Projects</h2>
+        </div>
+        <div>
+          <h2>Writing</h2>
+        </div>
+        <div>
+          <h2 className="mb-2">Experience</h2>
+          <div className="py-2 px-6 bg-white rounded-2xl">
+            <h3 className="mb-2">Empirical Foods</h3>
+            <div className="flex items-center">
+              <Briefcase className="mr-2" size={14} />
+              <span className="font-medium">Software Engineer Intern</span>
+            </div>
+            <div className="flex items-center">
+              <Calendar className="mr-2" size={14} />
+              <span>May 2023 - August 2024</span>
+            </div>
+            <div className="flex items-center mb-4">
+              <Code className="mr-2" size={14} />
+              <span>Power BI, Azure Logic Apps (Power Automate), C# Web APIs</span>
+            </div>
+            <p className="mb-4">Focused on report creation and API development across three internships: Summer '23, part-time Spring '24, and Summer '24. 40 hours a week in the summers.</p>
+            <div className="mb-4">
+              <h4 className="font-semibold mb-2">Key Achievement: Optimized Mislabeled Product Trackdown Process</h4>
+              <ul className="list-none pl-0 space-y-2">
+                <li className="flex items-start">
+                  <CheckCircle className="mr-2 text-neutral-700 flex-shrink-0 mt-1" size={14} />
+                  <span>Developed an automated system to replace manual next-day tracking of incorrectly labeled products from Dynamics AX 2009, saving 1,300 hours annually (25 hours/week)</span>
+                </li>
+                <li className="flex items-start">
+                  <Code className="mr-2 text-neutral-700 flex-shrink-0 mt-1" size={14} />
+                  <span>Designed and implemented a <span className="text-neutral-900">Data Warehouse</span> for product line scan queues, accompanied by a comprehensive <span className="text-neutral-900">Power BI report</span> and <span className="text-neutral-900">Power Automate solution</span></span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="py-2 px-6 bg-white rounded-2xl mt-2">
+            <h3 className="mb-2">Thompson Solutions Group</h3>
+            <div className="flex items-center">
+              <Briefcase className="mr-2" size={14} />
+              <span className="font-medium">Software Engineer Intern</span>
+            </div>
+            <div className="flex items-center">
+              <Calendar className="mr-2" size={14} />
+              <span>May 2022 - August 2022</span>
+            </div>
+            <div className="flex items-center mb-4">
+              <Code className="mr-2" size={14} />
+              <span>Full-stack ASP .NET | Agribusiness Software</span>
+            </div>
+            <p className="mb-4">Gained hands-on experience in Full-stack ASP .NET development, contributing to maintenance management software in the agribusiness sector. 20 hours a week.</p>
+          </div>
+        </div>
+        <div>
+          <h2>Education</h2>
+        </div>
+      </div>
     </div>
   );
 }
