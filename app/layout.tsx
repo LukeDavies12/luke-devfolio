7d663e2bf27fb3f736a87c124cde6e958e7d4830
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-09-26 16:49:42 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from "next";
 import "./globals.css";
 // import { GeistMono } from 'geist/font/mono';
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Personal Site - Luke Davies",
@@ -19,6 +20,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics/> 
     </html>
   );
 }
